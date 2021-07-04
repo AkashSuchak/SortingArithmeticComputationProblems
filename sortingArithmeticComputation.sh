79202ae999cd62988d06d3b4d1fff031caf7b545
AkashSuchak <suchak.akash@gmail.com>
2021-07-05 01:27:09 +0530

    ADD : Computation -> a + b / c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 
 #AUTHOR : AKASH SUCHAK
-#Compute - a * b + c
+#Compute - a + b / c
 
 #User Inputs
 read -p "Enter Value of a : " a
@@ -11,7 +11,7 @@ read -p "Enter Value of c : " c
 #Condition to check User Inputs are integer or not then we calculate
 if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]
 then
-	result=$(( a * b + c ))
+	result=$(( a + b / c ))
 	echo $result
 else
      echo "Wrong Input!! Enter Integers Only."
