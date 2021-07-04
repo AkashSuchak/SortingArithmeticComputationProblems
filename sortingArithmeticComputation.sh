f15d70d1cd79c788f7d946c972b38f879e6af81c
AkashSuchak <suchak.akash@gmail.com>
2021-07-05 01:13:27 +0530

    ADD : Computation -> a + b * c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,9 +1,18 @@
 #! /bin/bash -x
 
 #AUTHOR : AKASH SUCHAK
-#WRITE PROGRAM TO TAKE THREE INPUTS - A, B & C
+#Compute - a + b * c
 
 #User Inputs
 read -p "Enter Value of a : " a
 read -p "Enter Value of b : " b
 read -p "Enter Value of c : " c
+
+#Condition to check User Inputs are integer or not then we calculate
+if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]
+then
+	result=$(( a + b * c ))
+	echo $result
+else
+     echo "Wrong Input!! Enter Integers Only."
+fi
